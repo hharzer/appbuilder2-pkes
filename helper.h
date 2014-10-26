9c0f25bb59870e70a2360fd3eb7a407f55ef6a62
hharzer <hharzer@me.com>
2014-10-26 11:52:29 +0100

    Added new documentations

--- helper.h
+++ helper.h
@@ -5,18 +5,27 @@
 
 
 
-
+//! Typedef for an universial id 
 typedef unsigned char uid8;
+
+//! Simplification
 typedef unsigned char byte;
+
+//! Show that the stack's two bytes long
 typedef unsigned int twobytes;
+
+//! A generic callback used for everything, e.g. connect, disconnect, etc.
 typedef void (*gen_callback)(uid8, char *);
 
+
+//! Structure to save callbacks by id
 struct callback_dictionary
 {
     uid8 *id;
     gen_callback *callback;
 };
 
+//! Informations for the handshake
 struct handshake_informations {
 	char* phrase;
 	byte attempts;
